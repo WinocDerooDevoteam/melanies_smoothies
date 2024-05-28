2327a8b23e9028f438dfa83c1a34f4082be23140
WinocDerooDevoteam <winoc.deroo@devoteam.com>
2024-05-28 12:53:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,14 @@ if ingredients_list:
                 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
                 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """', '""" +name_on_order+ """')"""
 
-st.write(my_insert_stmt)
-# st.stop()
-time_to_insert = st.button('Submit Order')
-    
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
+        st.write(my_insert_stmt)
+        # st.stop()
+        time_to_insert = st.button('Submit Order')
+
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
         
