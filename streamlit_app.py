e6ca3fd2232fcc10884f2c33dcddf8f9511d43ab
WinocDerooDevoteam <winoc.deroo@devoteam.com>
2024-05-28 12:27:23 +0200

    Update streamlit_app.py
     # New section to diplay fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+        
+ # New section to diplay fruityvice nutrition information       
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
