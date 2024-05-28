65e012c16b83e20cd365d58ea13252d2cc60890c
WinocDerooDevoteam <winoc.deroo@devoteam.com>
2024-05-28 12:46:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,11 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
         for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ' '
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+                ingredients_string += fruit_chosen + ' '
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """', '""" +name_on_order+ """')"""
